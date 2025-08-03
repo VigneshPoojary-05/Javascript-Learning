d4a43dc3fcb93d4c097a5303280c32702deeb992
VigneshPoojary-05 <vigneshpoojary005@gmail.com>
2025-08-03 10:44:28 +0530

    Day-2 2nd Commit

---
+++ Day-2/Arithmetic.js
@@ -0,0 +1,27 @@
+// Arithmetic Operators
+
+console.log(7 + 2);
+console.log(7 - 2);
+console.log(7 * 2);
+console.log(7 / 2);
+console.log(7 % 2);
+console.log(2 ** 4);
+
+var x = 15;
+var y = 13;
+{
+    let a = 5;
+    let b = 3;
+    // var a=5;
+    // var b=3;
+    console.log(x + y);
+    console.log(a + b);
+    console.log(a - b);
+    console.log(a * b);
+    console.log(a / b);
+    console.log(a % b);
+    console.log(a ** b);
+} 
+console.log(x + y);
+console.log(a + b); //cann't accecible bcz, value is initialised inside the block by using let'. If we use 'var' we can access it outside the block;
+
