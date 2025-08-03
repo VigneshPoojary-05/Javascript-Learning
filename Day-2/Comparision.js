1029c2b86afb656af2217ef605d30b1a7dfcb111
VigneshPoojary-05 <vigneshpoojary005@gmail.com>
2025-08-03 12:34:34 +0530

    comaristion commit

---
+++ Day-2/Comparision.js
@@ -0,0 +1,18 @@
+// Loosely Equal and Not Equals
+console.log(5==5);
+console.log(5=="5")
+console.log(3=="5")
+
+console.log(5!=5);
+console.log(5!=="5")
+console.log(3!=="5")
+console.log(5!="3")
+
+
+// Strickly Equals and Not Equals
+console.log(5===5)
+console.log(5==="5")
+console.log("3"==="5")
+
+console.log(5!=="5")
+console.log(5!==5)
