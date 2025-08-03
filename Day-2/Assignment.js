bb89f270b886eec01621ccd6fb9c62b4739c91ce
VigneshPoojary-05 <vigneshpoojary005@gmail.com>
2025-08-03 12:05:13 +0530

    Next commit

---
+++ Day-2/Assignment.js
@@ -0,0 +1,22 @@
+let a=5;
+console.log(a);
+a+=5;//a=a+5
+console.log(a);
+
+a-=5; //a=a-5
+console.log(a);
+
+a*=3; //a=a*3
+console.log(a);
+
+a/=2; // a=a/2
+console.log(a);
+
+a+=0.5; //a=a+0.5
+console.log(a);
+
+a%=3; // a=a%3
+console.log(a);
+ 
+a**=3; //aa=a**3
+console.log(a);
