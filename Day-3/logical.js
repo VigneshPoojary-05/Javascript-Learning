81ab1b0e9b1f6be06b3bc9baf01498d049c798c1
VigneshPoojary-05 <vigneshpoojary005@gmail.com>
2025-08-04 18:43:33 +0530

    Day-3 1st commit

--- Day-3/logical.js
+++ Day-3/logical.js
@@ -1,1 +1,18 @@
-console.log(hello)
+console.log(true && true)
+console.log(false && true)
+console.log(true && false)
+console.log(false && false)
+
+console.log(true || true)
+console.log(false || true)
+console.log(true || false)
+console.log(false || false)
+
+console.log(!true)
+console.log(!false)
+
+let x = 100;
+console.log(x>=0 && x<=100)
+
+let y = 100;
+console.log(y>=0 || y<=100)
