e98a2ad8cb0622542c5d66a55adf9b1bd6bb37c9
VigneshPoojary-05 <vigneshpoojary005@gmail.com>
2025-08-22 20:12:38 +0530

    Day-15 last commit

--- Day-15/event_deligation.js
+++ Day-15/event_deligation.js
@@ -1,9 +1,9 @@
 let container = document.querySelector('.container')
-// container.childNodes.forEach((elem)=>{
-//     elem.addEventListener('click',()=>{
-//         console.log(elem.innerText)
-//     })
-// })
+container.childNodes.forEach((elem)=>{
+    elem.addEventListener('click',()=>{
+        console.log(elem.innerText)
+    })
+})
 
 container.addEventListener('click',(e)=>{
     if(e.target.className === 'box'){
