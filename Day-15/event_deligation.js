bc79aa3ec00337f9b63eef1bf1f36ee9aee3f20c
VigneshPoojary-05 <vigneshpoojary005@gmail.com>
2025-08-22 20:11:25 +0530

    Day-15 last commit

---
+++ Day-15/event_deligation.js
@@ -0,0 +1,12 @@
+let container = document.querySelector('.container')
+// container.childNodes.forEach((elem)=>{
+//     elem.addEventListener('click',()=>{
+//         console.log(elem.innerText)
+//     })
+// })
+
+container.addEventListener('click',(e)=>{
+    if(e.target.className === 'box'){
+        console.log(e.target.textContent)
+    }
+})
---
+++ Day-15/script.js
@@ -0,0 +1,56 @@
+let grandParent = document.querySelector('.grand-parent');
+let parent = document.querySelector('.parent');
+let child = document.querySelector('.child');
+
+// grandParent.addEventListener('click',()=>{
+//     alert('grandParent was clicked')
+// }, true)
+// grandParent.addEventListener('click',()=>{
+//     alert('grandParent was clicked')
+// })
+
+// parent.addEventListener('click',()=>{
+//     alert('parent was clicked')
+// }, true)
+// parent.addEventListener('click',()=>{
+//     alert('parent was clicked')
+// })
+
+// child.addEventListener('click',()=>{
+//     alert('Child was clicked')
+// }, true)
+// child.addEventListener('click',()=>{
+//     alert('Child was clicked')
+// })
+
+
+// grandParent.addEventListener('click',(e)=>{
+//     console.log('This is Grand Parent')
+//     console.log(e.target)
+//     console.log(e.currentTarget)
+// })
+
+// parent.addEventListener('click',(e)=>{
+//     console.log('This is Parent')
+//     console.log(e.target)
+//     console.log(e.currentTarget)
+//     e.stopImmediatePropagation()
+// })
+// parent.addEventListener('click',(e)=>{
+//     console.log('This is Another Parent')
+// })
+
+// child.addEventListener('click',(e)=>{
+//     console.log('This is Child')
+//     console.log(e.target)
+//     console.log(e.currentTarget)
+// })
+
+
+// parent.addEventListener('click',function(e){
+//     console.log('This is Parent')
+//     console.log(e.target)
+//     console.log(e.currentTarget)
+//     console.log(this.currentTarget)
+// })
+
