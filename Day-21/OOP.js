decfc1ad9d2142ade8a58af879ecb6107f33c323
VigneshPoojary-05 <vigneshpoojary005@gmail.com>
2025-09-05 16:47:24 +0530

    9th commit

--- Day-21/OOP.js
+++ Day-21/OOP.js
@@ -1,59 +1,62 @@
-// let date = new Date();
-// console.log(date.toLocaleString())
+let date = new Date();
+console.log(date.toLocaleString())
 
-// let Student = {
-//   name: "vignesh",
-//   age: 19,
-//   car: "BMW",
-//   introMySelf: function () {
-//     console.log(
-//       ` My name is ${this.name}, my age is ${this.age}, my car is ${this.car} `
-//     );
-//   },
-// };
-// Student.introMySelf();
+
+//  #1 Object Literals 
+
+let Student = {
+  name: "vignesh",
+  age: 19,
+  car: "BMW",
+  introMySelf: function () {
+    console.log(
+      ` My name is ${this.name}, my age is ${this.age}, my car is ${this.car} `
+    );
+  },
+};
+Student.introMySelf();
 
 
 //  #2 Factory Function 
 
-// function Student(name, age, car) {
-//   return {
-//     name: name,
-//     age: age,
-//     car: car,
-//     introMySelf: function () {
-//       console.log(
-//         ` My name is ${this.name}, my age is ${this.age}, my car is ${this.car} `
-//       );
-//     },
-//   };
-// }
+function Student(name, age, car) {
+  return {
+    name: name,
+    age: age,
+    car: car,
+    introMySelf: function () {
+      console.log(
+        ` My name is ${this.name}, my age is ${this.age}, my car is ${this.car} `
+      );
+    },
+  };
+}
 
-// let res = Student("Vignesh", 19, "Audi");
-// let res1 = Student("Gopal", 51, "BMW");
-// let res2 = Student("Yashoda", 45, "Ferrari");
+let res = Student("Vignesh", 19, "Audi");
+let res1 = Student("Gopal", 51, "BMW");
+let res2 = Student("Yashoda", 45, "Ferrari");
 
-// console.log(res);
-// console.log(res1);
-// console.log(res2);
+console.log(res);
+console.log(res1);
+console.log(res2);
 
-// res.introMySelf();
-// res1.introMySelf();
-// res2.introMySelf();
+res.introMySelf();
+res1.introMySelf();
+res2.introMySelf();
 
 
 // #3 Constructor Function 
 
-// function Student(name, age, car){
-//     this.name=name;
-//     this.age=age;
-//     this.car=car
-//     return this
-// }
-// let s1 = new Student("Vignesh",19,"BMW")
-// let s2 = new Student("Yashoda",49,"Ferrari")
-// console.log(s1)
-// console.log(s2)
+function Student(name, age, car){
+    this.name=name;
+    this.age=age;
+    this.car=car
+    return this
+}
+let s1 = new Student("Vignesh",19,"BMW")
+let s2 = new Student("Yashoda",49,"Ferrari")
+console.log(s1)
+console.log(s2)
 
 
 // #4 Class Syntax(ES6)
