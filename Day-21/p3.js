07a564e795b3b6a8d5b2f7fc615eab75d7066fca
VigneshPoojary-05 <vigneshpoojary005@gmail.com>
2025-09-05 11:21:46 +0530

    Day-21 4th commit

--- Day-21/p3.js
+++ Day-21/p3.js
@@ -18,3 +18,23 @@ function compose(...fns){
 }
 let result = compose(sub, mul, add)(6)
 console.log(result)
+
+let sanitise = str => str.replace(/[^a-z0-9 ]/gi, "") 
+let trim = str => str.trim();
+let capitalise = str => str.charAt(0).toUpperCase() + str.slice(1).toLowerCase();
+
+let input = "  vignesh@2003^kl &*";
+
+let clean = capitalise(trim(sanitise(input)))
+console.log(clean)
+
+function compose(...fns){
+    return function(val){
+        return fns.reduceRight((acc, curFn)=>{
+            return curFn(acc)
+        },val)
+    }
+}
+let com = compose(capitalise, trim, sanitise)
+console.log(com(input))
+
