c13c9e3bc07926e0dc675db7f9e02a2c6eb45190
VigneshPoojary-05 <vigneshpoojary005@gmail.com>
2025-09-05 20:17:18 +0530

    12th commit

--- Day-21/OOP3.js
+++ Day-21/OOP3.js
@@ -18,3 +18,21 @@ BMW.burnFuel()
 console.log(BMW)
 BMW.#Fuel = 50
 console.log(BMW)
+
+
+class BankAc{
+    #Balance=100;
+    constructor(holderName, Balance=100){
+        this.holderName=holderName;
+        this.#Balance=Balance
+    }
+    deposite(Balance){
+        this.#Balance+=Balance
+    }
+}
+let vigAc = new BankAc('Vignesh', 5000);
+console.log(vigAc)
+vigAc.deposite(1000)
+console.log(vigAc)
+vigAc.Balance = 10000;
+console.log(vigAc)
