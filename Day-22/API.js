5c4f7f3064dfb53d7da88c34d7c2431e20e29835
VigneshPoojary-05 <vigneshpoojary005@gmail.com>
2025-09-06 19:31:30 +0530

    Day-22 last commit commit

--- Day-22/API.js
+++ Day-22/API.js
@@ -1,2 +1,5 @@
-let response = fetch('https://jsonplaceholder.typicode.com/users');
-console.log(response)
+async function fetchData() {
+    let response = await fetch('https://newsapi.org/v2/everything?q=tesla&from=2025-08-06&sortBy=publishedAt&apiKey=f498ad77f5a94a7a9a25b3104232f37c');
+    console.log(await response.json());
+}
+fetchData();
