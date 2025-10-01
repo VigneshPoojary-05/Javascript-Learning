714d32f07b5f70dcf5aa2f754053722150abf023
VigneshPoojary-05 <vigneshpoojary005@gmail.com>
2025-10-01 17:17:20 +0530

    Day-24 first commit

---
+++ Day-24/script.js
@@ -0,0 +1,49 @@
+let taskInput = document.querySelector("#taskInput");
+let addBtn = document.querySelector("#addTaskBtn");
+let todoContainer = document.querySelector(".todoContainer");
+let API = "https://68bd2cf2227c48698f83e532.mockapi.io/api/v1/todos";
+
+addBtn.addEventListener("click", postData);
+
+async function fetchData() {
+  let response = await fetch(API);
+  let data = await response.json();
+
+  if(data){
+    todoContainer.innerHTML = "";
+    data.forEach((obj) => {
+        let div = document.createElement("div");
+        div.className = "todo";
+        div.innerHTML = `
+                  <p>${obj.text}</p>
+                  <div class="btns">
+                      <button>Edit</button>
+                      <button>Delete</button>
+                  </div>`;
+        todoContainer.append(div);
+    });
+  }
+}
+fetchData();
+
+async function postData() {
+
+    let value = taskInput.value;
+    
+    let objData = {
+        text: value.trim()
+    };
+    
+  let response = await fetch(API, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(objData),
+  });
+
+  if(response.status === 201){
+    fetchData();
+  }
+}
+postData();
