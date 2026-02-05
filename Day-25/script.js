a13a5a42fdebb3b518a38b0de1868d9323305e9d
VigneshPoojary-05 <vigneshpoojary005@gmail.com>
2026-02-05 14:59:01 +0530

    update

---
+++ Day-25/script.js
@@ -0,0 +1,50 @@
+let taskInput = document.querySelector("#taskInput");
+let addBtn = document.querySelector("#addTaskBtn");
+let todoContainer = document.querySelector(".todoContainer");
+let API = "https://68bd2cf2227c48698f83e532.mockapi.io/api/v1/todos";
+
+addBtn.addEventListener("click", postData);
+
+
+//Get Data from server
+async function fetchData() {
+    let response = await fetch(API);
+    let data = await response.json();
+    if(data){
+    todoContainer.innerHTML='';
+    data.forEach((obj) => {
+      let div = document.createElement("div");
+      div.className = "todo";
+      div.innerHTML = `
+        <p>${obj.text}</p>
+        <div class="btns">
+        <button>Edit</button>
+          <button>Delete</button>
+          </div>`
+          todoContainer.append(div);
+        });
+  }
+}
+
+
+
+//Post Data in server
+async function postData() {
+    let value = taskInput.value;
+    let objData = {
+        text: value.trim()
+    };
+    
+  let response = await fetch(API, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(objData),
+  });
+
+  if(response.status === 201){
+    fetchData();
+  }
+}
+fetchData();
---
+++ Day-26/s.js
@@ -0,0 +1,3 @@
+let arr=[1,2,3,4,5];
+let newArr=[...arr,45,3,5];
+console.log(...newArr)
